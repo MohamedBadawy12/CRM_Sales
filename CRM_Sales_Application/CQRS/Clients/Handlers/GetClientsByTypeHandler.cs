f1c73cb0368eacff56325136fe1891d84fbf366e
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-15 00:16:25 +0300

    update handler and queries of client

--- CRM_Sales_Application/CQRS/Clients/Handlers/GetClientsByTypeHandler.cs
+++ CRM_Sales_Application/CQRS/Clients/Handlers/GetClientsByTypeHandler.cs
@@ -6,22 +6,23 @@ using MediatR;
 
 namespace CRM_Sales_Application.CQRS.Clients.Handlers
 {
-    public class GetAllClientsHandler : IRequestHandler<GetAllClientsQuery, IEnumerable<ClientDto>>
+    public class GetClientsByTypeHandler : IRequestHandler<GetClientsByTypeQuery, IEnumerable<ClientDto>>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public GetAllClientsHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public GetClientsByTypeHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<ClientDto>> Handle(
-            GetAllClientsQuery request, CancellationToken cancellationToken)
+            GetClientsByTypeQuery request, CancellationToken cancellationToken)
         {
             var clients = await _unitOfWork.Clients.GetAllWithIncludesAsync();
-            return _mapper.Map<IEnumerable<ClientDto>>(clients);
+            var filtered = clients.Where(c => c.Type == request.Type);
+            return _mapper.Map<IEnumerable<ClientDto>>(filtered);
         }
     }
 }
