45f38c3b99760e1ccaafacc26ae5cd6005bb1c61
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-15 00:25:13 +0300

    update

--- CRM_Sales_MVC/Program.cs
+++ CRM_Sales_MVC/Program.cs
@@ -1,5 +1,8 @@
 using CRM_Sales_Application.Extensions;
+using CRM_Sales_Core.Entites;
+using CRM_Sales_Infrastructure.Data;
 using CRM_Sales_Infrastructure.Persistence.Extensions;
+using Microsoft.AspNetCore.Identity;
 namespace CRM_Sales_MVC
 {
     public class Program
@@ -17,6 +20,30 @@ namespace CRM_Sales_MVC
 
             builder.Services.AddApplication();
 
+            builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Auth/Login";
+                options.AccessDeniedPath = "/Auth/Login";
+                options.ExpireTimeSpan = TimeSpan.FromHours(8);
+            });
+
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(8);
+                options.Cookie.HttpOnly = true;
+            });
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -30,13 +57,14 @@ namespace CRM_Sales_MVC
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
             app.Run();
         }
