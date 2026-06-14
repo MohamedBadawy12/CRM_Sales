571b5b6c3229e78742c3fdb97a82cc800c504ecb
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-15 00:20:26 +0300

    Add Auth Controller

---
+++ CRM_Sales_MVC/Controllers/AuthController.cs
@@ -0,0 +1,91 @@
+﻿using CRM_Sales_Application.DTOs;
+using CRM_Sales_Core.Entites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_Sales_MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET: Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Dashboard");
+            return View();
+        }
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _signInManager.PasswordSignInAsync(
+                model.Email, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Dashboard");
+
+            ModelState.AddModelError("", "Email or Password is wrong");
+            return View(model);
+        }
+        // GET: Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Dashboard");
+            return View();
+        }
+
+        // POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var user = new AppUser
+            {
+                FullName = model.FullName,
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(model);
+        }
+
+        // POST: Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+    }
+}
