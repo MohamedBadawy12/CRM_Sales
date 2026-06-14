82ed64895aa2e70b681a26b560337677259af735
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-15 00:18:05 +0300

    update IGeneric

--- CRM_Sales_Core/Interfaces/IGenericRepository.cs
+++ CRM_Sales_Core/Interfaces/IGenericRepository.cs
@@ -3,6 +3,7 @@
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllWithIncludesAsync();
         Task<T?> GetByIdAsync(Guid id);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
