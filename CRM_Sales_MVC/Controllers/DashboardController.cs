a24442fc91e58774d0cf598f5f0afbf6e3006e98
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-15 00:21:27 +0300

    update dasboard controller

---
+++ CRM_Sales_MVC/Controllers/DashboardController.cs
@@ -0,0 +1,45 @@
+﻿using CRM_Sales_Application.CQRS.Clients.Queries;
+using CRM_Sales_Application.CQRS.Projects.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_Sales_MVC.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly IMediator _mediator;
+        public DashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allClients = await _mediator.Send(new GetAllClientsQuery());
+            var allProjects = await _mediator.Send(new GetAllProjectsQuery());
+
+            var today = DateTime.UtcNow.Date;
+            var clientsList = allClients.ToList();
+
+            ViewBag.TotalClients = allClients.Count();
+            ViewBag.WalkClients = allClients.Count(c => c.Type == "Walk");
+            ViewBag.FollowClients = allClients.Count(c => c.Type == "Follow");
+            ViewBag.TotalProjects = allProjects.Count();
+
+            ViewBag.TodayWalk = clientsList
+                .Count(c => c.Type == "Walk" && c.CreatedAt.Date == today);
+            ViewBag.TodayFollow = clientsList
+                .Count(c => c.Type == "Follow" && c.CreatedAt.Date == today);
+            ViewBag.TodayTotal = clientsList
+                .Count(c => c.CreatedAt.Date == today);
+
+            ViewBag.RecentClients = clientsList
+                .OrderByDescending(c => c.CreatedAt)
+                .Take(10)
+                .ToList();
+
+            return View();
+        }
+    }
+}
