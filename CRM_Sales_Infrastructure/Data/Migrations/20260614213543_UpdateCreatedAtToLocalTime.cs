005462a3a8611bcbc7bc147cee9475c36b87ad42
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-19 06:05:33 +0300

    update

---
+++ CRM_Sales_Infrastructure/Data/Migrations/20260614213543_UpdateCreatedAtToLocalTime.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM_Sales_Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCreatedAtToLocalTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
