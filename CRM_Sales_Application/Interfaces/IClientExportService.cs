8cf27c300558fb358315279edf45e159a1169b58
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-19 17:16:08 +0300

    Add Interface IClientExportService

---
+++ CRM_Sales_Application/Interfaces/IClientExportService.cs
@@ -0,0 +1,10 @@
+﻿using CRM_Sales_Application.DTOs;
+
+namespace CRM_Sales_Application.Interfaces
+{
+    public interface IClientExportService
+    {
+        byte[] ExportToExcel(IEnumerable<ClientDto> clients);
+        byte[] ExportToPdf(IEnumerable<ClientDto> clients);
+    }
+}
