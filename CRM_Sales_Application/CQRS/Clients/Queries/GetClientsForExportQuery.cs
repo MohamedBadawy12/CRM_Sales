bdc8129d2a6d421ee0b78ed4dd2f35ef9198d670
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-19 17:15:42 +0300

    Adding GetClientsForExportQuery

---
+++ CRM_Sales_Application/CQRS/Clients/Queries/GetClientsForExportQuery.cs
@@ -0,0 +1,12 @@
+﻿using CRM_Sales_Application.DTOs;
+using MediatR;
+
+namespace CRM_Sales_Application.CQRS.Clients.Queries
+{
+    public record GetClientsForExportQuery(
+        string? Ids,
+        string Type,
+        string? Search,
+        int? Month,
+        int? Year) : IRequest<IEnumerable<ClientDto>>;
+}
