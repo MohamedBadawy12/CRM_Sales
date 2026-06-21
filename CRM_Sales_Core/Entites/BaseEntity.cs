389211bbaf7b2124a30459793be81d5f01dd2ee8
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-21 21:29:31 +0300

    Adding DeletedAt

--- CRM_Sales_Core/Entites/BaseEntity.cs
+++ CRM_Sales_Core/Entites/BaseEntity.cs
@@ -6,8 +6,20 @@
         public DateTime CreatedAt { get; protected set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; protected set; }
         public bool IsDeleted { get; protected set; } = false;
+        public DateTime? DeletedAt { get; protected set; }
 
         public void SetUpdatedAt() => UpdatedAt = DateTime.UtcNow;
-        public void SoftDelete() => IsDeleted = true;
+        public void SoftDelete()
+        {
+            IsDeleted = true;
+            DeletedAt = DateTime.Now;
+        }
+
+        public void Restore()
+        {
+            IsDeleted = false;
+            DeletedAt = null;
+            UpdatedAt = DateTime.Now;
+        }
     }
 }
