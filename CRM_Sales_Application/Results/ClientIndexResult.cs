e734ea613b738bd4113cb052c9ec24ff70f0461b
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-21 21:29:53 +0300

    Add ClientResult

---
+++ CRM_Sales_Application/Results/ClientIndexResult.cs
@@ -0,0 +1,9 @@
+﻿using CRM_Sales_Application.DTOs;
+
+namespace CRM_Sales_Application.Results
+{
+    public class ClientIndexResult
+    {
+        public IEnumerable<ClientDto> Clients { get; set; }
+    }
+}
