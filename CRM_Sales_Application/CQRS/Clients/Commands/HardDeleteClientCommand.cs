56480a14cd001b80f9aec16660e99e6b18ff6ab6
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-21 21:30:50 +0300

    Adding Delete and restore clients

---
+++ CRM_Sales_Application/CQRS/Clients/Commands/HardDeleteClientCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CRM_Sales_Application.CQRS.Clients.Commands
+{
+    public record HardDeleteClientCommand(Guid Id) : IRequest<bool>;
+}
---
+++ CRM_Sales_Application/CQRS/Clients/Commands/RestoreClientCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CRM_Sales_Application.CQRS.Clients.Commands
+{
+    public record RestoreClientCommand(Guid Id) : IRequest<bool>;
+}
