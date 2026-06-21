793d22a4aca6416fd7ce41132e8de58ef1efc5e5
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-21 21:31:58 +0300

    Update IGeneric

--- CRM_Sales_Core/Interfaces/IGenericRepository.cs
+++ CRM_Sales_Core/Interfaces/IGenericRepository.cs
@@ -4,9 +4,13 @@
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> GetAllWithIncludesAsync();
+        Task<IEnumerable<T>> GetAllDeletedAsync();
         Task<T?> GetByIdAsync(Guid id);
+        Task<T?> GetByIdIncludingDeletedAsync(Guid id);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(Guid id); // Soft Delete
+        Task RestoreAsync(Guid id);
+        Task HardDeleteAsync(Guid id);
     }
 }
