4f4c772a70fa2e1ea056cc2ba58e1cd55d0b354d
MohamedBadawy12 <m.badawy2442002@gmail.com>
2026-06-21 21:32:20 +0300

    AddDeleted Migration

---
+++ CRM_Sales_Infrastructure/Data/Migrations/20260621181636_AddDeletedAtToBaseEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM_Sales_Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeletedAtToBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Teams",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "SalesAgents",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Projects",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Clients",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "SalesAgents");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Clients");
+        }
+    }
+}
